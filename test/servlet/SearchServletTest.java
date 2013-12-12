2969d95daa371ea00bf97c28d831fec5bfb2104a
Jiho Kim <go2zo@nate.com>
2013-12-13 00:15:37 +0900

    Add ant build scripts

---
+++ test/servlet/SearchServletTest.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertEquals;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+
+public class SearchServletTest {
+
+//	@Test
+//	public void test() throws ServletException, IOException {
+//		HttpServletRequest request = mock(HttpServletRequest.class);
+//		HttpServletResponse response = mock(HttpServletResponse.class);    
+//
+//        when(request.getParameter("keyword")).thenReturn("java");
+//
+//        new SearchServlet().doPost(request, response);
+//
+//        assertEquals("text/html", response.getContentType());
+//	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+
+}
